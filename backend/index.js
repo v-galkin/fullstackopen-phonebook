fbcd1ce3118a93e32e239681715a400853095ee5
v-galkin <galkinvl111@gmail.com>
2026-08-22 07:14:51 +1200

    Add .gitattributes to enforce LF line endings

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,26 @@
-const express = require('express');
-const morgan = require('morgan');
-const app = express();
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
 
-const connectToDatabase = require('./db/connect');
-const phonebookRouter = require('./controllers/phonebook');
-const { errorHandler, unknownEndpoint } = require('./utils/middleware');
+const connectToDatabase = require('./db/connect')
+const phonebookRouter = require('./controllers/phonebook')
+const { errorHandler, unknownEndpoint } = require('./utils/middleware')
 
-morgan.token('body', (request) => JSON.stringify(request.body));
+morgan.token('body', (request) => JSON.stringify(request.body))
 
-connectToDatabase();
+connectToDatabase()
 
-app.use(express.json());
-app.use(express.static('dist'));
+app.use(express.json())
+app.use(express.static('dist'))
 
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-app.use('/api/phonebook', phonebookRouter);
+app.use('/api/phonebook', phonebookRouter)
 
-app.use(errorHandler);
-app.use(unknownEndpoint);
+app.use(errorHandler)
+app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+  console.log(`Server is running on port ${PORT}`)
+})
--- backend/utils/middleware.js
+++ backend/utils/middleware.js
@@ -2,19 +2,16 @@ const errorHandler = (error, request, response, next) => {
   console.error(error.message)
 
   if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' });
+    return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
-    return response.status(400).send({ error: error.message });
+    return response.status(400).send({ error: error.message })
   }
 
   next(error)
-};
+}
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' });
-};
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
-module.exports = { 
-                    errorHandler, 
-                    unknownEndpoint 
-                };
+module.exports = { errorHandler, unknownEndpoint,}

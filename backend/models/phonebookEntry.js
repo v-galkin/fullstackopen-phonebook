f3c0329cc8bf1fb7eeccf4c4c42148a76d05f3b0
v-galkin <galkinvl111@gmail.com>
2026-08-22 06:21:46 +1200

    uibuild

--- backend/models/phonebookEntry.js
+++ backend/models/phonebookEntry.js
@@ -2,7 +2,16 @@ const mongoose = require('mongoose');
 
 const phonebookSchema = new mongoose.Schema({
     name: String,
-    number: String,
+    number: {
+        type:string,
+        validate: {
+            validator: function(v) {
+                return /^\d{2,3}-\d+$/.test(v);
+            },
+            message: props => `${props.value} is not a valid phone number`
+        },
+        required: [true, 'User phone number required']
+    }
 })
 
 phonebookSchema.set('toJSON', {
